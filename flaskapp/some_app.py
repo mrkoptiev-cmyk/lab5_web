a353ec71523021c2e885de8dc50a814ed783e214
mrkoptiev-cmyk <mrkoptiev@gmail.com>
2026-09-26 18:39:55 +0500

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -5,5 +5,5 @@ app=Flask(__name__)
 def hello():
   return " <html><head></head> <body> Hello World! </body></html>"
 if __name__=="__main__":
-  app.run(host='127.0.0.1',port=5000)
+  app.run(host='127.0.0.1',port=5000
   
